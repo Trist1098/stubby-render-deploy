5b447aba384053d143ab5f7687ebb0440c8ddd7e
ganlijie11 <ganlijie11@gmail.com>
2026-05-17 23:42:36 +0800

    Add CRUD operations for calendar events and enhance event management UI

--- src/routers/Home.router.js
+++ src/routers/Home.router.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const jwtMiddleware = require('../middlewares/jwt.middleware');
-const { getCalendarEvents } = require('../controllers/Home.controller');
+const {
+  getCalendarEvents,
+  createCalendarEvent,
+  updateCalendarEvent,
+  deleteCalendarEvent,
+} = require('../controllers/Home.controller');
 
 // ##############################################################
 // FEATURE: Home & Dashboard
 // ##############################################################
 
 router.get('/calendar', jwtMiddleware.verifyToken, getCalendarEvents);
+router.post('/calendar', jwtMiddleware.verifyToken, createCalendarEvent);
+router.put('/calendar/:id', jwtMiddleware.verifyToken, updateCalendarEvent);
+router.delete('/calendar/:id', jwtMiddleware.verifyToken, deleteCalendarEvent);
 
 module.exports = router;
